3a38681d54157418f6833324583acbebdb971486
Tonmoy Roy <rxdevs100@gmail.com>
2021-11-11 01:00:15 +0600

    products section

---
+++ src/Pages/Home/Product/Product.js
@@ -0,0 +1,38 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import React from "react";
+
+const Product = ({ product }) => {
+    const { name, description, price, img } = product;
+    return (
+        <Grid  item xs={12} md={4}>
+            <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Price: {price} USD
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Buy Now
+        </Button>
+      </CardActions>
+    </Card>           
+        </Grid>
+    );
+};
+
+export default Product ;
--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,8 +1,35 @@
-import React from "react";
+import { Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('./fakeData.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+        
+    }, []);
+
     return (
         <div>
-            <h3>Products here</h3>
+            <Box sx={{ flexGrow: 1 }}>
+                <Container>
+                <Typography sx={{ my:4, textAlign:'center' }} variant="h4" component="div">
+         OUR BEST PRODUCTS
+        </Typography>
+                    <Grid container spacing={2}>
+                        {
+                            products.map(product =>
+                                <Product
+                                    key={product.name}
+                                    product={product}
+                                ></Product>
+                            )}
+                    </Grid>
+                </Container>
+            </Box>
         </div>
     );
 };
