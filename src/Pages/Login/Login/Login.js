4823e67153f1f3e8d3b586529cd604c432083883
Tonmoy Roy <rxdevs100@gmail.com>
2022-01-10 13:33:25 +0600

    Login and Register page UI updated

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,16 +1,49 @@
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { Alert, Button, CircularProgress, Container, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
+import { Alert, Button, Container, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { NavLink, useHistory, useLocation } from "react-router-dom";
 import useAuth from '../../../Hooks/useAuth';
+import GoogleButton from 'react-google-button'
+import Navigation from '../../Shared/Navigation/Navigation.js';
+import Footer from '../../Shared/Footer/Footer.js';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const Login = () => {
     const [userLoginData, setUserLoginData] = useState({});
     const { user, logInUser, signInWithGoogle, loading, authError } = useAuth();
+	const [progress, setProgress] = React.useState(0);
+	const [buffer, setBuffer] = React.useState(10);
     const location = useLocation();
     const history = useHistory();
+	// Linear Progress bar
+	
+	const progressRef = React.useRef(() => {});
+	  React.useEffect(() => {
+		progressRef.current = () => {
+		  if (progress > 100) {
+			setProgress(0);
+			setBuffer(10);
+		  } else {
+			const diff = Math.random() * 10;
+			const diff2 = Math.random() * 10;
+			setProgress(progress + diff);
+			setBuffer(progress + diff + diff2);
+		  }
+		};
+	});
+	React.useEffect(() => {
+    const timer = setInterval(() => {
+      progressRef.current();
+    }, 500);
 
+    return () => {
+		  clearInterval(timer);
+		};
+	}, []);
+	
     // MUI Input Adornments State
     const [values, setValues] = React.useState({
         password: '',
@@ -51,10 +84,21 @@ const Login = () => {
     }
 
     return (
+		<>
+		<Navigation></Navigation>
         <Container>
             <Grid container spacing={0}>
-                <Grid item sx={{ mt: 10, mx: 'auto', backgroundColor: '#fff' }} xs={12} md={4}>
-                    <Typography sx={{color:'#7bb519', textAlign: 'center',mb:3 }} variant="h4" gutterBottom>Login</Typography>
+                <Grid item sx={{ mt: 8, mx: 'auto', backgroundColor: '#fff' }} xs={10} sm={8} md={5} lg={5}>
+					<Paper elevation={6}>
+					<Box sx={{py:5}}>
+                    <Typography sx={{color:'#000000', textAlign: 'center',mb:2 }} variant="h5" gutterBottom>Get's started</Typography>
+					<Box sx={{textAlign:'center',mb:2}}>
+						Don't have an account?
+						<NavLink
+                            style={{ textDecoration: 'none',color:'#7bb519',marginLeft:'10px' }}
+                            to="/register">Register Now
+						</NavLink>
+					</Box>
                     <form onSubmit={handleLogInSubmit} style={{ width: '80%', margin: ' 0 auto' }}>
 
                         <TextField sx={{ width: '100%', mb: 2 }}
@@ -62,10 +106,11 @@ const Login = () => {
                             label="Email"
                             name="email"
                             onBlur={handleInputOnBlur}
-                            variant="outlined" />
+                            variant="outlined" 
+							required />
                         
                         <FormControl sx={{ width: '100%', mb: 2 }} variant="outlined">
-                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <InputLabel required htmlFor="outlined-adornment-password">Password</InputLabel>
                             <OutlinedInput
                                 id="outlined-adornment-password"
                                 type={values.showPassword ? 'text' : 'password'}
@@ -88,24 +133,26 @@ const Login = () => {
                                 onBlur={handleInputOnBlur}
                             />
                         </FormControl>
-                        <Button sx={{backgroundColor:'#7bb519',width: '100%', mb: 2}} color="success" type="submit" variant="contained">Login</Button>
-                        <NavLink
-                            style={{ textDecoration: 'none' }}
-                            to="/register">
-                            <Button color="success" style={{ width: '100%', mx: 'auto' }} variant="text">New User? Please Register</Button>
-                        </NavLink>
-                        {loading && <CircularProgress/>}
+                        <Button sx={{backgroundColor:'#7bb519',width: '100%',height:'50px', mb: 2, textTransform:'capitalize',fontSize:'16px'}} color="success" type="submit" variant="contained">Login</Button>
+						<GoogleButton label="Continue with Google" style={{width: '100%', marginBottom:'10px',textAlign:'center'}} type="dark" onClick={handleGoogleSignIn} />
+                        
+                        {loading && <LinearProgress variant="buffer" value={progress} sx={{my:3}} valueBuffer={buffer} />}
                         {
                             user?.email && <Alert severity="success">Login successfully!</Alert>
                         }
                         {
                             authError && <Alert severity="error">{authError}</Alert>
                         }
-                        <Button sx={{backgroundColor:'#7bb519',width: '100%', mt: 2}} color="success" onClick={handleGoogleSignIn} variant="contained">Continue With Google</Button>
+                        
+						
                     </form>
+					</Box>
+					</Paper>
                 </Grid>
             </Grid>
         </Container>
+		<Footer></Footer>
+		</>
     );
 };
 
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,15 +1,52 @@
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { Alert, Button, CircularProgress, Container, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
+import { Alert, Button, Container, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { NavLink, useHistory, useLocation } from "react-router-dom";
 import useAuth from '../../../Hooks/useAuth';
+import GoogleButton from 'react-google-button'
+import Navigation from '../../Shared/Navigation/Navigation.js';
+import Footer from '../../Shared/Footer/Footer.js';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 const Register = () => {
     const [userRegisterData, setUserRegisterData] = useState({});
     const { user, registerUser, signInWithGoogle, loading, authError } = useAuth();
+	const [progress, setProgress] = React.useState(0);
+	const [buffer, setBuffer] = React.useState(10);
     const location = useLocation();
     const history = useHistory();
+	
+	// Linear Progress bar
+	const progressRef = React.useRef(() => {});
+	  React.useEffect(() => {
+		progressRef.current = () => {
+		  if (progress > 100) {
+			setProgress(0);
+			setBuffer(10);
+		  } else {
+			const diff = Math.random() * 10;
+			const diff2 = Math.random() * 10;
+			setProgress(progress + diff);
+			setBuffer(progress + diff + diff2);
+		  }
+		};
+	});
+	React.useEffect(() => {
+    const timer = setInterval(() => {
+      progressRef.current();
+    }, 500);
+
+    return () => {
+		  clearInterval(timer);
+		};
+	}, []);
 
     // MUI Input Adornments State
     const [values, setValues] = React.useState({
@@ -55,12 +92,22 @@ const Register = () => {
     }
 
     return (
+		<>
+		<Navigation></Navigation>
         <Container>
             <Grid container spacing={0}>
-                <Grid item sx={{ mt: 10, mx: 'auto', backgroundColor: '#fff' }} xs={12} md={4}>
-                    <Typography sx={{color:'#7bb519', textAlign: 'center',mb:3 }} variant="h4" gutterBottom>Create An Account</Typography>
-                    {
-                        !loading && <form onSubmit={handleRegisterSubmit} style={{ width: '80%', margin: ' 0 auto' }}>
+                <Grid item sx={{ mt: 8, mx: 'auto', backgroundColor: '#fff' }} xs={10} sm={8} md={5} lg={5}>
+					<Paper elevation={6}>
+					<Box sx={{py:5}}>
+                    <Typography sx={{color:'#000000', textAlign: 'center',mb:2 }} variant="h5" gutterBottom>Welcome to SmartTechShop</Typography>
+					<Box sx={{textAlign:'center',mb:2}}>
+						Have an account?
+						<NavLink
+                            style={{ textDecoration: 'none',color:'#7bb519',marginLeft:'10px' }}
+                            to="/login">Login Now
+						</NavLink>
+					</Box>
+						<form onSubmit={handleRegisterSubmit} style={{ width: '80%', margin: ' 0 auto' }}>
 
                             <TextField sx={{ width: '100%', mb: 2 }}
                                 id="outlined-basic"
@@ -68,17 +115,19 @@ const Register = () => {
                                 name="name"
                                 type="text"
                                 onBlur={handleInputOnBlur}
-                                variant="outlined" />
+								required
+                                variant="outlined"								/>
                             <TextField sx={{ width: '100%', mb: 2 }}
                                 id="outlined-basic"
                                 label="Email"
                                 name="email"
                                 onBlur={handleInputOnBlur}
+								required
                                 variant="outlined" />
-                        
                             <FormControl sx={{ width: '100%', mb: 2 }} variant="outlined">
-                                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                                <InputLabel required htmlFor="outlined-adornment-password">Password</InputLabel>
                                 <OutlinedInput
+									required
                                     id="outlined-adornment-password"
                                     type={values.showPassword ? 'text' : 'password'}
                                     value={values.password}
@@ -106,28 +155,31 @@ const Register = () => {
                                 name="password2"
                                 type="password"
                                 onBlur={handleInputOnBlur}
-                                variant="outlined" />
-                            <Button sx={{backgroundColor:'#7bb519',width: '100%', mb: 2}} type="submit" variant="contained">Register Now</Button>
-                            <NavLink
-                                style={{ textDecoration: 'none' }}
-                                to="/login">
-                                <Button color="success" style={{ width: '100%', mx: 'auto' }} variant="text">Existing User? Please Login</Button>
-                            </NavLink>
+                                variant="outlined"
+								required />
+							<FormGroup>
+								<FormControlLabel required control={<Checkbox />} label="I agree to the Terms of Use and Privacy Policy" />
+							</FormGroup>
+                            <Button sx={{backgroundColor:'#7bb519',width: '100%',height:'50px', my: 2, textTransform:'capitalize',fontSize:'16px'}} color="success" type="submit" variant="contained">Register</Button>
+							<GoogleButton label="Continue with Google" style={{width: '100%', marginBottom:'10px',textAlign:'center'}} type="dark" onClick={handleGoogleSignIn} />
+							{
+								loading && <LinearProgress variant="buffer" value={progress} sx={{my:3}} valueBuffer={buffer} />
+							}
                             {
                                 user?.email && <Alert severity="success">Login successfully!</Alert>
                             }
                             {
                                 authError && <Alert severity="error">{authError}</Alert>
                             }
-                            <Button sx={{backgroundColor:'#7bb519',width: '100%', mt: 2}} color="success" onClick={handleGoogleSignIn} variant="contained">Continue With Google</Button>
-                        </form>
-                    }           
-                    {
-                        loading && <CircularProgress />
-                    }
+                        </form>          
+                    
+					</Box>
+					</Paper>
                 </Grid>
             </Grid>
         </Container>
+		<Footer></Footer>
+		</>
     );
 };
 
