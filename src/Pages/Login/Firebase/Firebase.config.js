1801cf7fe97ea8c695c843830afeb78fcd69efd7
Tonmoy Roy <rxdevs100@gmail.com>
2024-06-04 20:26:18 +0600

    fix: Remove firebase configs info

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -1,11 +1,5 @@
 // Web app's Firebase configuration
 const firebaseConfig = {
-  //   apiKey: "AIzaSyBoB9CuEEifD2oUKOumNixSOTCHFB8QcKI",
-  //   authDomain: "smart-tech-authentication.firebaseapp.com",
-  //   projectId: "smart-tech-authentication",
-  //   storageBucket: "smart-tech-authentication.appspot.com",
-  //   messagingSenderId: "1055653591600",
-  //   appId: "1:1055653591600:web:0527dafe5f1b88083e8bf5",
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
